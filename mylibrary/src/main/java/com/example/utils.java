0fa45a730ef63139b4dee823c8fc8f7f7c214495
doupeixu <1015209027@qq.com>
2018-05-12 10:41:31 +0800

    Initial commit

---
+++ mylibrary/src/main/java/com/example/utils.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * @version V1.0
+ * @class
+ * @anthor dpx
+ * @time 2018/5/12 0012 10:34
+ */
+public class utils {
+    public String getString(){
+        return "hello github!";
+    }
+}
